69e2500f2b8b28f3a042d0d87813d9d551b5eb2c
Kira Gelaz <kiragelaz@yandex.ru>
2025-03-14 11:48:28 +0300

    fix types for mypy

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,7 +3,7 @@ from .extentions import db, migrate
 from .config import Config
 
 
-def create_app(config_class=Config) -> Flask:
+def create_app(config_class=Config) -> object:
     app = Flask(__name__)
     app.config.from_object(config_class)
 
