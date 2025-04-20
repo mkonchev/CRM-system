22ffb5f40bb709b8d13069738428338a7c348d5b
mkonchev <maksim1901m@gmail.com>
2025-04-20 14:43:44 +0300

    Create new column in CarModel

--- dct/apps/car/models/CarModel.py
+++ dct/apps/car/models/CarModel.py
@@ -1,21 +1,26 @@
 from django.db import models
 
 from apps.car.models.admins.CarModelAdmin import CarModelAdmin
+from config.constants import NULLABLE
 
 
 class Car(models.Model):
     ModelAdmin = CarModelAdmin
 
-    number = models.CharField(verbose_name='Номер', max_length=10, default='')
+    number = models.CharField(verbose_name='Номер',
+                              max_length=10,
+                              default='',
+                              **NULLABLE)
     mark = models.CharField(verbose_name='Марка', max_length=50, default='')
     model = models.CharField(verbose_name='Модель', max_length=50, default='')
     vin = models.CharField(verbose_name='VIN/Номер кузова',
                            max_length=100,
                            default='')
+    year = models.PositiveIntegerField(verbose_name='Год выпуска', **NULLABLE)
     owner = models.ForeignKey('core.User',
                               verbose_name='ID владельца',
                               on_delete=models.CASCADE,
-                              related_name='cars',
+                              related_name='car_owner',
                               )
 
     class Meta:
--- dct/apps/car/models/admins/CarModelAdmin.py
+++ dct/apps/car/models/admins/CarModelAdmin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 
 
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ["number", "mark", "model", "vin", "owner"]
+    list_display = ["number", "mark", "model", "vin", "year", "owner"]
