234505ab43eb06fdb1543757f60e5fb31aa5c1ab
mkonchev <maksim1901m@gmail.com>
2025-04-20 15:13:13 +0300

    add to Order: date_start, date_end, worker selection

--- dct/apps/order/models/OrderModel.py
+++ dct/apps/order/models/OrderModel.py
@@ -7,19 +7,25 @@ class Order(models.Model):
     ModelAdmin = OrderModelAdmin
     # list_display = ["owner", "car", "services", "checkpoints", "worker"]
     owner = models.ForeignKey('core.User',
-                              verbose_name='ID владельца',
+                              verbose_name='Владелец',
                               on_delete=models.CASCADE,
                               related_name='order_owner',
+                              help_text='Автозаполнение при выборе машины',
                               **NULLABLE)
     # Нужно поменять owner related_name
     car = models.ForeignKey('car.Car',
-                            verbose_name='ID машины',
+                            verbose_name='Машина',
                             on_delete=models.CASCADE,
                             related_name='orders')
     worker = models.ForeignKey('core.User',
-                               verbose_name='ID работника',
+                               verbose_name='Работник',
                                on_delete=models.CASCADE,
-                               related_name='orders')
+                               related_name='orders',
+                               limit_choices_to={'is_active': True,
+                                                 'role': 1})
+    start_date = models.DateTimeField(verbose_name='Начало работ',
+                                      auto_now=True)
+    end_date = models.DateTimeField(verbose_name='Окончание работ', **NULLABLE)
 
     class Meta:
         verbose_name = 'Заявка'
--- dct/apps/order/models/admins/OrderModelAdmin.py
+++ dct/apps/order/models/admins/OrderModelAdmin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 
 
 class OrderModelAdmin(admin.ModelAdmin):
-    list_display = ["owner", "car", "worker"]
+    list_display = ["owner", "car", "worker", "start_date", "end_date"]
