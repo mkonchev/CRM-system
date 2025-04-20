a9b9026766fde1591a6cd20de35e855df26556d0
mkonchev <maksim1901m@gmail.com>
2025-04-21 02:21:30 +0300

    fix OrderSerializer add workstatus :)

--- dct/apps/api/serializers/OrderSerializer.py
+++ dct/apps/api/serializers/OrderSerializer.py
@@ -1,19 +1,14 @@
 from rest_framework import serializers
 
-# from apps.api.serializers.CarSerializer import CarSerializer
-# from apps.api.serializers.UserSerializer import UserSerializer
 from apps.api.serializers.WorkSerializer import WorkSerializer
 from apps.order.models.OrderModel import Order
 
 
 class OrderSerializer(serializers.ModelSerializer):
 
-    # owner = UserSerializer()
-    # car = CarSerializer()
     works = WorkSerializer(many=True)
-    # worker = UserSerializer()
     workstatus = serializers.PrimaryKeyRelatedField(
-        many=True, read_only=True, source='order_work_status')
+        many=True, read_only=True, source='order_works')
 
     class Meta:
         model = Order
