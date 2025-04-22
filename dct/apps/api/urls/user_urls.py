7469ab4a520cf354a900d8bd0f4ec8f41262f0eb
Kikos1337 <skalisusov@inbox.ru>
2025-04-22 19:45:33 +0300

    add update_user, create_user, delete_user

--- dct/apps/api/urls/user_urls.py
+++ dct/apps/api/urls/user_urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from apps.api.views.user_views import user_list_view, user_by_id
+from apps.api.views import user_views
 
 urlpatterns = [
-    path('', user_list_view),
-    path('<int:pk>', user_by_id),
+    path('', user_views.user_list_view),
+    path('<int:pk>', user_views.user_by_id),
+    path('create', user_views.add_user),
+    path('<int:pk>/update', user_views.update_user),
+    path('<int:pk>/delete', user_views.delete_user),
 ]
