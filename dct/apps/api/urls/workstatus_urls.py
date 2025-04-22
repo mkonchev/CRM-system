e257698cfe36ace36b312ff28b95aed35c4f8f2d
Kikos1337 <skalisusov@inbox.ru>
2025-04-22 19:58:17 +0300

    add update_workstatus, create_workstatus, delete_workstatus

--- dct/apps/api/urls/workstatus_urls.py
+++ dct/apps/api/urls/workstatus_urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from apps.api.views.workstatus_views import (
-    workstatus_list_view, workstatus_by_id)
+from apps.api.views import workstatus_views
 
 
 urlpatterns = [
-    path('', workstatus_list_view),
-    path('<int:pk>', workstatus_by_id),
+    path('', workstatus_views.workstatus_list_view),
+    path('<int:pk>', workstatus_views.workstatus_by_id),
+    path('create', workstatus_views.add_workstatus),
+    path('<int:pk>/update', workstatus_views.update_workstatus),
+    path('<int:pk>/delete', workstatus_views.delete_workstatus),
 ]
--- dct/apps/api/views/workstatus_views.py
+++ dct/apps/api/views/workstatus_views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -18,3 +18,39 @@ def workstatus_by_id(request, pk):
     workstatus = Workstatus.objects.get(pk=pk)
     serializer = Workstatus(workstatus)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def add_workstatus(request):
+    workstatus = WorkstatusSerializer(data=request.data)
+
+    # if user.objects.filter(**request.data).exists():
+    #     raise serializers.ValidationError('This data already exists')
+
+    if workstatus.is_valid():
+        workstatus.save()
+        return Response(data=workstatus.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def update_workstatus(request, pk):
+    workstatus = Workstatus.objects.get(pk=pk)
+    workstatus = WorkstatusSerializer(instance=workstatus, data=request.data)
+
+    if workstatus.is_valid():
+        workstatus.save()
+        return Response(data=workstatus.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_workstatus(request, pk):
+    if Workstatus.objects.filter(pk=pk).exists():
+        workstatus = Workstatus.objects.get(pk=pk)
+        workstatus.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+    else:
+        raise serializers.ValidationError('Not found this data')
