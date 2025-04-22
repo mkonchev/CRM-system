e92a05c503451839ee3a16323001bd604bf3831c
Kikos1337 <skalisusov@inbox.ru>
2025-04-22 17:12:51 +0300

    comment out other serializers to OrderSerializers

--- dct/apps/api/serializers/OrderSerializer.py
+++ dct/apps/api/serializers/OrderSerializer.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 
-from apps.api.serializers.WorkSerializer import WorkSerializer
+# from apps.api.serializers.WorkSerializer import WorkSerializer
 from apps.order.models.OrderModel import Order
 
 
 class OrderSerializer(serializers.ModelSerializer):
 
-    works = WorkSerializer(many=True)
-    workstatus = serializers.PrimaryKeyRelatedField(
-        many=True, read_only=True, source='order_works')
+    # works = WorkSerializer(many=True)
+    # workstatus = serializers.PrimaryKeyRelatedField(
+    #     many=True, read_only=True, source='order_works')
 
     class Meta:
         model = Order
