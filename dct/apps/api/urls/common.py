edadf2fa45fe2f0e1e962af15c233780ce05971c
mkonchev <maksim1901m@gmail.com>
2025-04-22 16:15:47 +0300

    add api_view

--- dct/apps/api/urls/common.py
+++ dct/apps/api/urls/common.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
+from apps.api.views import api_view
 
 
 app_name = 'api'
 
 urlpatterns = [
+    path('', api_view.api_overview),
     path('user/', include('apps.api.urls.user_urls')),
     path('car/', include('apps.api.urls.car_urls')),
     path('work/', include('apps.api.urls.work_urls')),
