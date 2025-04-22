fc64bf44423ffc54f792b278341e473b12ba626c
Kikos1337 <skalisusov@inbox.ru>
2025-04-22 19:51:10 +0300

    add update_work, create_work, delete_work

--- dct/apps/api/urls/work_urls.py
+++ dct/apps/api/urls/work_urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from apps.api.views.work_views import work_list_view, work_by_id
+from apps.api.views import work_views
 
 
 urlpatterns = [
-    path('', work_list_view),
-    path('<int:pk>', work_by_id),
+    path('', work_views.work_list_view),
+    path('<int:pk>', work_views.work_by_id),
+    path('create', work_views.add_work),
+    path('<int:pk>/update', work_views.update_work),
+    path('<int:pk>/delete', work_views.delete_work),
 ]
--- dct/apps/api/views/work_views.py
+++ dct/apps/api/views/work_views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -18,3 +18,39 @@ def work_by_id(request, pk):
     work = Work.objects.get(pk=pk)
     serializer = Work(work)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def add_work(request):
+    work = WorkSerializer(data=request.data)
+
+    # if user.objects.filter(**request.data).exists():
+    #     raise serializers.ValidationError('This data already exists')
+
+    if work.is_valid():
+        work.save()
+        return Response(data=work.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def update_work(request, pk):
+    work = Work.objects.get(pk=pk)
+    upd_work = WorkSerializer(instance=work, data=request.data)
+
+    if upd_work.is_valid():
+        upd_work.save()
+        return Response(data=upd_work.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_work(request, pk):
+    if Work.objects.filter(pk=pk).exists():
+        work = Work.objects.get(pk=pk)
+        work.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+    else:
+        raise serializers.ValidationError('Not found this data')
