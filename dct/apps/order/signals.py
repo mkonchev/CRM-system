11cf07755410ba09b5a892f87bcf1fe982cbb458
mkonchev <maksim1901m@gmail.com>
2025-05-01 16:20:49 +0300

    add update_order_completion_status_signal

--- dct/apps/order/signals.py
+++ dct/apps/order/signals.py
@@ -1,7 +1,5 @@
-from django.db.models.signals import pre_save, m2m_changed
+from django.db.models.signals import pre_save
 from apps.order.models.OrderModel import Order
-from apps.workstatus.models.WorkstatusModel import Workstatus
-from apps.workstatus.models.consts import WorkStatusChoice
 
 
 def auto_fill_owner_id(instance, sender, **kwargs):
@@ -10,33 +8,4 @@ def auto_fill_owner_id(instance, sender, **kwargs):
         instance.owner = instance.car.owner
 
 
-# def create_workstatuses_for_order_items(sender, instance, action, **kwargs):
-#     if action == "post_add":
-#         Workstatus.objects.filter(order=instance).delete()
-#         Workstatus.objects.bulk_create([
-#             Workstatus(
-#                 work=work,
-#                 order=instance,
-#                 status=WorkStatusChoice.none
-#             )
-#             for work in instance.works.all()
-#         ])
-# def update_order_completion_status(sender, instance, **kwargs):
-#     order = instance.order
-#     all_works = order.items.all()
-#     if not all_works.exists():
-#         order.is_completed = False
-#         order.save(update_fields=['is_completed'])
-#         return
-#     all_completed = not any(
-#         work.status != WorkStatusChoice.completed
-#         for work in all_works
-#     )
-#     if order.is_completed != all_completed:
-#         order.is_completed = all_completed
-#         order.save(update_fields=['is_completed'])
-
-
-# m2m_changed.connect(update_order_completion_status,
-#                     sender=Workstatus)
 pre_save.connect(auto_fill_owner_id, sender=Order)
--- dct/apps/workstatus/apps.py
+++ dct/apps/workstatus/apps.py
@@ -5,3 +5,6 @@ class WorkstatusConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.workstatus'
     verbose_name = 'Статус работы'
+
+    def ready(self):
+        import apps.workstatus.signals  # noqa: F401
