61a0ce3f8af6fa46219d172ba5f298c0ad9e97b2
mkonchev <maksim1901m@gmail.com>
2025-05-01 19:10:20 +0300

    fix order views

--- dct/apps/api/serializers/OrderSerializer.py
+++ dct/apps/api/serializers/OrderSerializer.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 
-from apps.work.models.WorkModel import Work
+# from apps.work.models.WorkModel import Work
 from apps.order.models.OrderModel import Order
 
 
 class OrderSerializer(serializers.ModelSerializer):
 
-    works = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Work.objects.all())
-    workstatus = serializers.PrimaryKeyRelatedField(
-        many=True, read_only=True, source='order_works')
+    # works = serializers.PrimaryKeyRelatedField(
+    #     many=True, queryset=Work.objects.all())
+    # workstatus = serializers.PrimaryKeyRelatedField(
+    #     many=True, read_only=True, source='order_works')
 
     class Meta:
         model = Order
--- dct/config/urls/common.py
+++ dct/config/urls/common.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include('apps.api.urls.common', namespace='api'))
 ]
 
