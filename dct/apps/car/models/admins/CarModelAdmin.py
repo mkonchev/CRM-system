86e2192713869a58f46a0ad9f35258d2789ca9c1
mkonchev <maksim1901m@gmail.com>
2025-05-01 16:03:23 +0300

    add search_fields to admin

--- dct/apps/car/models/admins/CarModelAdmin.py
+++ dct/apps/car/models/admins/CarModelAdmin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ["number", "mark", "model", "vin", "year", "owner"]
+    search_fields = ("mark", "model")
--- dct/apps/workstatus/models/admins/WorkstatusModelAdmin.py
+++ dct/apps/workstatus/models/admins/WorkstatusModelAdmin.py
@@ -4,3 +4,4 @@ from django.contrib import admin
 class WorkstatusModelAdmin(admin.ModelAdmin):
     list_display = ['work', 'order', 'status', 'amount', 'fix_price']
     list_editable = ['status']
+    search_fields = ("work", "order")
