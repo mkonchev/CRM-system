cde7490ee1629f2aa474176f7e5a2886f3d43167
Kikos1337 <skalisusov@inbox.ru>
2025-05-27 02:28:02 +0300

    create ursl tests for workstatus

--- dct/apps/api/tests/tests_work/tests_ursl.py
+++ dct/apps/api/tests/tests_work/tests_ursl.py
@@ -3,24 +3,24 @@ from django.test import TestCase
 from apps.api.views import work_views
 
 
-class UserUrlsTest(TestCase):
+class WorkUrlsTest(TestCase):
 
-    def test_car_list_url(self):
+    def test_work_list_url(self):
         url = '/api/work/'
         self.assertEqual(resolve(url).func, work_views.work_list_view)
 
-    def test_car_detail_urls(self):
+    def test_work_detail_urls(self):
         url = f'/api/work/{1}'
         self.assertEqual(resolve(url).func, work_views.work_by_id_view)
 
-    def test_car_create_url(self):
+    def test_work_create_url(self):
         url = '/api/work/create'
         self.assertEqual(resolve(url).func, work_views.add_work_view)
 
-    def test_car_update_url(self):
+    def test_work_update_url(self):
         url = f'/api/work/{1}/update'
         self.assertEqual(resolve(url).func, work_views.update_work_view)
 
-    def test_car_delete_url(self):
+    def test_work_delete_url(self):
         url = f'/api/work/{1}/delete'
         self.assertEqual(resolve(url).func, work_views.delete_work_view)
