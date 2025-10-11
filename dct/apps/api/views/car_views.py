52205ac267c9db8d460253a543a333de00190d2d
mkonchev <maksim1901m@gmail.com>
2025-10-11 04:05:27 +0300

    fix

--- dct/apps/api/views/car_views.py
+++ dct/apps/api/views/car_views.py
@@ -1,5 +1,5 @@
 from rest_framework import status, serializers, permissions
-# from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
