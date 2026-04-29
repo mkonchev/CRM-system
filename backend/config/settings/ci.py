56d7ebcae5823435198da8e9a1280891788cf420
mkonchev <maksim1901m@gmail.com>
2026-04-29 18:18:40 +0300

    fix tests

---
+++ backend/config/settings/ci.py
@@ -0,0 +1,32 @@
+from .common import *
+
+DEBUG = False
+SECRET_KEY = 'django-insecure-ci-key'
+
+STATICFILES_DIRS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_EAGER_PROPAGATES = True
+CELERY_BROKER_URL = 'memory://'
+CELERY_RESULT_BACKEND = 'memory://'
+
+REDIS_URL = None
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
